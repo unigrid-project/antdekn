84d319fa999c370abfb19b01d3b03c8e84f0e4b5
Adam Waldenberg <adam@unigrid.org>
2020-08-25 20:33:10 +0200

    Update license terms and add license addendum in COPYING.addendum

--- backend/src/main/java/org/unigrid/antdekm/wallet/model/block/BaseBlock.java
+++ backend/src/main/java/org/unigrid/antdekm/wallet/model/block/BaseBlock.java
@@ -4,14 +4,20 @@
 
   This program is free software: you can redistribute it and/or modify it under the
   terms of the GNU Affero General Public License as published by the Free Software
-  Foundation, either version 3 of the License, or (at your option) any later version.
+  Foundation, either version 3 of the License, or (at your option) any later
+  version. This program also amends the license with additional terms as outlined
+  in the addendum of the license terms.
 
   This program is distributed in the hope that it will be useful, but WITHOUT ANY
   WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
-  PARTICULAR PURPOSE.  See the GNU Affero General Public License for more details.
+  PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
 
-  You should have received a copy of the GNU Affero General Public License along
-  with this program.  If not, see <https://www.gnu.org/licenses/>.
+  The full license terms of this program are outlined in the files COPYING and
+  COPYING.addendum, found in the root directory of this project repository as
+  seen on <https://github.com/unigrid-project/antdekn>. Updated contact information
+  can also be found on this project page. If you did not received a copy of the
+  GNU Affero General Public License along with this program, please see
+  <https://www.gnu.org/licenses/>.
 */
 
 package org.unigrid.antdekm.wallet.model.block;
